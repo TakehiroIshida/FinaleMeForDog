441e8ed2aae039c39fc8fb2cab76d7c32dc7c499
Takehiro Ishida <icda420takehero@gmail.com>
2026-02-16 18:59:09 +0900

    bedgraphの出力ディレクトリを変更

--- test/BedToBigWig.sh
+++ test/BedToBigWig.sh
@@ -1,6 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 IN="out/prediction.all.parts128.gz"
 CHRSIZES="test/data/BH01.chrom.sizes"
 PREFIX="BH01"
+
+# 出力先（FinaleMeForDog/out 配下）
+OUTDIR="out/bedgraph"
+mkdir -p "$OUTDIR"
 
 # 予測（predict）
 zcat "$IN" \
@@ -8,21 +15,21 @@ zcat "$IN" \
   | sed 's/^chrM/MT/; s/^chr//' \
   | cut -f1-3,4 \
   | bedtools sort -i stdin \
-  > "${PREFIX}.methy_perc_predict.bedgraph"
+  > "${OUTDIR}/${PREFIX}.methy_perc_predict.bedgraph"
 
 zcat "$IN" \
   | grep -v '^#' \
   | sed 's/^chrM/MT/; s/^chr//' \
   | cut -f1-3,5 \
   | bedtools sort -i stdin \
-  > "${PREFIX}.methy_count_predict.bedgraph"
+  > "${OUTDIR}/${PREFIX}.methy_count_predict.bedgraph"
 
 zcat "$IN" \
   | grep -v '^#' \
   | sed 's/^chrM/MT/; s/^chr//' \
   | cut -f1-3,6 \
   | bedtools sort -i stdin \
-  > "${PREFIX}.total_count_predict.bedgraph"
+  > "${OUTDIR}/${PREFIX}.total_count_predict.bedgraph"
 
 # 観測（obs）
 zcat "$IN" \
@@ -30,27 +37,27 @@ zcat "$IN" \
   | sed 's/^chrM/MT/; s/^chr//' \
   | cut -f1-3,7 \
   | bedtools sort -i stdin \
-  > "${PREFIX}.methy_perc_obs.bedgraph"
+  > "${OUTDIR}/${PREFIX}.methy_perc_obs.bedgraph"
 
 zcat "$IN" \
   | grep -v '^#' \
   | sed 's/^chrM/MT/; s/^chr//' \
   | cut -f1-3,8 \
   | bedtools sort -i stdin \
-  > "${PREFIX}.methy_count_obs.bedgraph"
+  > "${OUTDIR}/${PREFIX}.methy_count_obs.bedgraph"
 
 zcat "$IN" \
   | grep -v '^#' \
   | sed 's/^chrM/MT/; s/^chr//' \
   | cut -f1-3,9 \
   | bedtools sort -i stdin \
-  > "${PREFIX}.total_count_obs.bedgraph"
+  > "${OUTDIR}/${PREFIX}.total_count_obs.bedgraph"
 
-# bedGraph -> bigWig
-bedGraphToBigWig "${PREFIX}.methy_perc_predict.bedgraph" "$CHRSIZES" "${PREFIX}.methy_perc_predict.bw"
-bedGraphToBigWig "${PREFIX}.methy_count_predict.bedgraph" "$CHRSIZES" "${PREFIX}.methy_count_predict.bw"
-bedGraphToBigWig "${PREFIX}.total_count_predict.bedgraph" "$CHRSIZES" "${PREFIX}.total_count_predict.bw"
+# bedGraph -> bigWig（同じ out/bedgraph に出す）
+bedGraphToBigWig "${OUTDIR}/${PREFIX}.methy_perc_predict.bedgraph" "$CHRSIZES" "${OUTDIR}/${PREFIX}.methy_perc_predict.bw"
+bedGraphToBigWig "${OUTDIR}/${PREFIX}.methy_count_predict.bedgraph" "$CHRSIZES" "${OUTDIR}/${PREFIX}.methy_count_predict.bw"
+bedGraphToBigWig "${OUTDIR}/${PREFIX}.total_count_predict.bedgraph" "$CHRSIZES" "${OUTDIR}/${PREFIX}.total_count_predict.bw"
 
-bedGraphToBigWig "${PREFIX}.methy_perc_obs.bedgraph" "$CHRSIZES" "${PREFIX}.methy_perc_obs.bw"
-bedGraphToBigWig "${PREFIX}.methy_count_obs.bedgraph" "$CHRSIZES" "${PREFIX}.methy_count_obs.bw"
-bedGraphToBigWig "${PREFIX}.total_count_obs.bedgraph" "$CHRSIZES" "${PREFIX}.total_count_obs.bw"
+bedGraphToBigWig "${OUTDIR}/${PREFIX}.methy_perc_obs.bedgraph" "$CHRSIZES" "${OUTDIR}/${PREFIX}.methy_perc_obs.bw"
+bedGraphToBigWig "${OUTDIR}/${PREFIX}.methy_count_obs.bedgraph" "$CHRSIZES" "${OUTDIR}/${PREFIX}.methy_count_obs.bw"
+bedGraphToBigWig "${OUTDIR}/${PREFIX}.total_count_obs.bedgraph" "$CHRSIZES" "${OUTDIR}/${PREFIX}.total_count_obs.bw"
